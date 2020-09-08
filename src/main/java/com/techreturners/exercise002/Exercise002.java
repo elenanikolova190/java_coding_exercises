4ccf92ca7511a2ba3e818e3b7e76abc1b3233252
elenanikolova190 <70112811+elenanikolova190@users.noreply.github.com>
2020-09-08 23:28:09 +0100

    Complete exercise 2

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,12 +2,20 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        return false;
+    public boolean isFromManchester( Person person ) {
+        boolean fromManchester = false;
+        if(person.getCity().equals("Manchester")){
+            fromManchester =  true;
+        } 
+      return fromManchester;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        return false;
+    public boolean canWatchFilm( Person person, int ageLimit) {
+        boolean canWatch =  false;
+        if(person.getAge() >= ageLimit){
+            canWatch = true;
+        }
+        return canWatch;
     }
     
 }
